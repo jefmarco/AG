cba5f6516d1325c344eee64dc924b27fca131f95
jeffry marte collado <jeffry07@hotmail.com>
2017-09-03 16:54:50 +0200

    test randomalg

---
+++ Algoritmo.Tests/RandomAlgorithmTest/RandomAlgorithmtest.cs
@@ -0,0 +1,29 @@
+﻿using Algoritmo.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo.Tests.RandomAlgorithmTest
+{
+    [TestFixture]
+    public class RandomAlgorithmtest
+    {
+        [Test]
+        public void Test()
+        {
+            RandomAlgorithm p = new RandomAlgorithm();
+            var batches = p.GenerateRandomBatches(5);
+        }
+
+
+
+
+
+
+
+
+    }
+}
