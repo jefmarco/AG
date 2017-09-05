02e679679d5e821b6bce8db74869741f949a1444
jeffry marte collado <jeffry07@hotmail.com>
2017-09-06 00:45:35 +0200

    moh

---
+++ Algoritmo.Tests/Helper/MachineObjectiveHelperTest.cs
@@ -0,0 +1,14 @@
+﻿using Algoritmo.Core.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo.Tests.Helper
+{
+    class MachineObjectiveHelperTest
+    {
+        MachineObjectiveHelper m = new MachineObjectiveHelper(List);
+    }
+}
